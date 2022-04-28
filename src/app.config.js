08c80a2b6dbbeff0f049238de14749bc8f9571a8
roissticker <stefan@sticker.es>
2022-04-28 17:00:27 +0000

    Third Change

--- src/app.config.js
+++ src/app.config.js
@@ -4,7 +4,7 @@
  * DO NOT store state here - must be part of a stateless service
  */
 class AppConfig {
-    team = 'Iron Main';
+    team = 'Iron Main 2';
     version = '1.0.0';
 }
 module.exports = AppConfig;
